efddcb52ae0656428b19868c78be0aeb81fda096
tommyb82 <thomasbunting@gmail.com>
2020-10-05 10:58:52 +0100

    Remove commented out ES policy block until comprehensively understood

--- terraform/modules/elasticsearch/main.tf
+++ terraform/modules/elasticsearch/main.tf
@@ -27,24 +27,3 @@ resource "aws_elasticsearch_domain" "main" {
     aws_iam_service_linked_role.es,
   ]
 }
-
-# resource "aws_elasticsearch_domain_policy" "main" {
-#   domain_name = aws_elasticsearch_domain.main.domain_name
-#
-#   access_policies = <<POLICIES
-# {
-#     "Version": "2012-10-17",
-#     "Statement": [
-#         {
-#             "Action": "es:*",
-#             "Principal": "*",
-#             "Effect": "Allow",
-#             "Condition": {
-#                 "IpAddress": {"aws:SourceIp": "127.0.0.1/32"}
-#             },
-#             "Resource": "${aws_elasticsearch_domain.example.arn}/*"
-#         }
-#     ]
-# }
-# POLICIES
-# }
