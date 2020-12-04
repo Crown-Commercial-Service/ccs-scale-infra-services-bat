f94ca3e6c210e56383800500cd9d75620065e231
Anthony McKernan <anthonymckernan@gmail.com>
2020-12-04 11:50:07 +0000

    create directory structure for cnet data

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -58,3 +58,50 @@ resource "aws_s3_bucket" "product-import" {
 
   tags = merge(module.globals.project_resource_tags, { AppType = "S3" })
 }
+
+resource "aws_s3_bucket_object" "nightly_updates" {
+  bucket = aws_s3_bucket.cnet.id
+  key    = "nightly_updates/"
+  acl    = "private"
+  source = "/dev/null"
+
+  tags = merge(module.globals.project_resource_tags, { AppType = "S3" })
+}
+
+resource "aws_s3_bucket_object" "nightly_updates_new" {
+  bucket = aws_s3_bucket.cnet.id
+  key    = "nightly_updates/new/"
+  acl    = "private"
+  source = "/dev/null"
+
+  tags = merge(module.globals.project_resource_tags, { AppType = "S3" })
+}
+
+resource "aws_s3_bucket_object" "nightly_updates_error" {
+  bucket = aws_s3_bucket.cnet.id
+  key    = "nightly_updates/error/"
+  acl    = "private"
+  source = "/dev/null"
+
+  tags = merge(module.globals.project_resource_tags, { AppType = "S3" })
+}
+
+resource "aws_s3_bucket_object" "nightly_updates_processing" {
+  bucket = aws_s3_bucket.cnet.id
+  key    = "nightly_updates/processing/"
+  acl    = "private"
+  source = "/dev/null"
+
+  tags = merge(module.globals.project_resource_tags, { AppType = "S3" })
+}
+
+resource "aws_s3_bucket_object" "nightly_updates_done" {
+  bucket = aws_s3_bucket.cnet.id
+  key    = "nightly_updates/done/"
+  acl    = "private"
+  source = "/dev/null"
+
+  tags = merge(module.globals.project_resource_tags, { AppType = "S3" })
+}
+
+
