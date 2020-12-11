dce92dee3a71217100940b672698c4fb2eb5c159
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2020-12-11 13:38:42 +0000

    SINF-235 - added SBX8

---
+++ terraform/environments/sbx8/main.tf
@@ -0,0 +1,36 @@
+#########################################################
+# Environment: SBX8
+#
+# Deploy SCALE resources
+#########################################################
+terraform {
+  backend "s3" {
+    bucket         = "scale-terraform-state"
+    key            = "ccs-scale-infra-services-bat-sbx8"
+    region         = "eu-west-2"
+    dynamodb_table = "scale_terraform_state_lock"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+locals {
+  environment = "sbx8"
+}
+
+data "aws_ssm_parameter" "aws_account_id" {
+  name = "account-id-${lower(local.environment)}"
+}
+
+module "deploy" {
+  source              = "../../modules/configs/deploy-all"
+  aws_account_id      = data.aws_ssm_parameter.aws_account_id.value
+  environment         = local.environment
+  rollbar_env         = local.environment
+  ecr_image_id_spree  = "latest"
+  ecr_image_id_client = "latest"
+}
