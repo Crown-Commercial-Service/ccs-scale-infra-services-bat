edb368659a1a9f223a6d0d23445c333bacc53ffd
Anthony McKernan <anthonymckernan@gmail.com>
2021-01-28 14:23:58 +0000

    Change the size of the virus-scan-service ec2 instance in INT,SBX8 and NFT environments

--- terraform/environments/int/main.tf
+++ terraform/environments/int/main.tf
@@ -27,25 +27,23 @@ data "aws_ssm_parameter" "aws_account_id" {
 }
 
 module "deploy" {
-  source                    = "../../modules/configs/deploy-all"
-  aws_account_id            = data.aws_ssm_parameter.aws_account_id.value
-  environment               = local.environment
-  rollbar_env               = local.environment
-  client_cpu                = 2048
-  client_memory             = 4096 #t2.large - 8GB available
-  client_ec2_instance_type  = "t2.large"
-  spree_cpu                 = 4096
-  spree_memory              = 8192 #t2.xlarge - 16GB available
-  spree_ec2_instance_type   = "t2.xlarge"
-  sidekiq_cpu               = 4096
-  sidekiq_memory            = 8192 #t2.xlarge - 16GB available
-  sidekiq_ec2_instance_type = "t2.xlarge"
-  memcached_node_type       = "cache.t3.medium"
-  redis_node_type           = "cache.t3.medium"
-
-  # default values for s3-virus-scan-service subject to change based on testing on DEV
+  source                          = "../../modules/configs/deploy-all"
+  aws_account_id                  = data.aws_ssm_parameter.aws_account_id.value
+  environment                     = local.environment
+  rollbar_env                     = local.environment
+  client_cpu                      = 2048
+  client_memory                   = 4096 #t2.large - 8GB available
+  client_ec2_instance_type        = "t2.large"
+  spree_cpu                       = 4096
+  spree_memory                    = 8192 #t2.xlarge - 16GB available
+  spree_ec2_instance_type         = "t2.xlarge"
+  sidekiq_cpu                     = 4096
+  sidekiq_memory                  = 8192 #t2.xlarge - 16GB available
+  sidekiq_ec2_instance_type       = "t2.xlarge"
+  memcached_node_type             = "cache.t3.medium"
+  redis_node_type                 = "cache.t3.medium"
   ecr_image_id_s3_virus_scan      = "latest"
-  s3_virus_scan_cpu               = 1024
-  s3_virus_scan_memory            = 2048
-  s3_virus_scan_ec2_instance_type = "t2.medium"
+  s3_virus_scan_cpu               = 2048
+  s3_virus_scan_memory            = 4096
+  s3_virus_scan_ec2_instance_type = "t2.large"
 }
--- terraform/environments/nft/main.tf
+++ terraform/environments/nft/main.tf
@@ -27,22 +27,26 @@ data "aws_ssm_parameter" "aws_account_id" {
 }
 
 module "deploy" {
-  source                    = "../../modules/configs/deploy-all"
-  aws_account_id            = data.aws_ssm_parameter.aws_account_id.value
-  environment               = local.environment
-  rollbar_env               = local.environment
-  ecr_image_id_spree        = "latest"
-  ecr_image_id_client       = "latest"
-  client_cpu                = 4096
-  client_memory             = 8192        #t2.xlarge - 16GB available
-  client_ec2_instance_type  = "t2.xlarge" # NB: Som's initial design was 4/8, Ravi approved use of t2.xlarge as nearest instance size
-  spree_cpu                 = 4096
-  spree_memory              = 15360 #16GB available - save 1GB for the instance (or increase to t2.xlarge)
-  spree_ec2_instance_type   = "t2.xlarge"
-  sidekiq_cpu               = 4096
-  sidekiq_memory            = 15360 #16GB available - save 1GB for the instance (or increase to t2.xlarge)
-  sidekiq_ec2_instance_type = "t2.xlarge"
-  memcached_node_type       = "cache.m4.large"
-  redis_node_type           = "cache.m4.large"
-  az_names                  = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  source                          = "../../modules/configs/deploy-all"
+  aws_account_id                  = data.aws_ssm_parameter.aws_account_id.value
+  environment                     = local.environment
+  rollbar_env                     = local.environment
+  ecr_image_id_spree              = "latest"
+  ecr_image_id_client             = "latest"
+  client_cpu                      = 4096
+  client_memory                   = 8192        #t2.xlarge - 16GB available
+  client_ec2_instance_type        = "t2.xlarge" # NB: Som's initial design was 4/8, Ravi approved use of t2.xlarge as nearest instance size
+  spree_cpu                       = 4096
+  spree_memory                    = 15360 #16GB available - save 1GB for the instance (or increase to t2.xlarge)
+  spree_ec2_instance_type         = "t2.xlarge"
+  sidekiq_cpu                     = 4096
+  sidekiq_memory                  = 15360 #16GB available - save 1GB for the instance (or increase to t2.xlarge)
+  sidekiq_ec2_instance_type       = "t2.xlarge"
+  memcached_node_type             = "cache.m4.large"
+  redis_node_type                 = "cache.m4.large"
+  az_names                        = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  ecr_image_id_s3_virus_scan      = "latest"
+  s3_virus_scan_cpu               = 2048
+  s3_virus_scan_memory            = 4096
+  s3_virus_scan_ec2_instance_type = "t2.large"
 }
